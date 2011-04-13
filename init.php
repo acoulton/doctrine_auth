8f09b326baf775f8af0125823c4a24975ef89935
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-04-13 22:28:58 +0100

    Moved auth route to doctrine_auth module

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+defined('SYSPATH') or die('No direct script access.');
+
+Route::set('auth','<action>(/<token>)',
+        array('action'=>'signin|signout|register|reset|activate|account'))
+        ->defaults(array(
+            'controller' => 'auth',
+        ));
